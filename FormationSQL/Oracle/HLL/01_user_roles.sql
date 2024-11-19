e02fcec66bae3186fce940e6470987555e997110
LeMarechalDeFer <blanchot@et.esiea.fr>
2024-11-19 06:42:36 +0100

    script user, audit, permition less

--- FormationSQL/Oracle/HLL/01_user_roles.sql
+++ FormationSQL/Oracle/HLL/01_user_roles.sql
@@ -2,50 +2,67 @@
 -- docker exec -it oracle-database sqlplus hotel_app/"3X?c5K/w6E3d%R{x7u}"@//localhost:1521/FREEPDB1
 -- dans le futur, utiliser oracle wallet pour sécuriser les mots de passe
 
-SPOOL /opt/oracle/scripts/setup/initUser_$(date +%Y%m%d%H%M%S).log;
+SPOOL /tmp/setup/initUser_$(date +%Y%m%d%H%M%S).log;
 
 CONNECT / AS SYSDBA;
 ALTER SESSION SET CONTAINER = FREEPDB1;
 
 
--- USER hotel_admin
-CREATE USER hotel_admin IDENTIFIED BY "AdminSecurePassword"
-DEFAULT TABLESPACE users
-QUOTA UNLIMITED ON users; 
+DECLARE
+    err_code NUMBER;
+    err_msg VARCHAR2(4000);
+BEGIN
+    -- EXECUTE IMMEDIATE 'BEGIN';
 
-GRANT CONNECT, RESOURCE TO hotel_admin;
+    -- USER hotel_admin
+    EXECUTE IMMEDIATE '
+        CREATE USER hotel_admin IDENTIFIED BY "AdminSecurePassword"
+        DEFAULT TABLESPACE users
+        QUOTA UNLIMITED ON users';
+    EXECUTE IMMEDIATE 'GRANT CONNECT, RESOURCE TO hotel_admin'; 
 
--- USER hotel_app
-CREATE USER hotel_app IDENTIFIED by "3X?c5K/w6E3d%R{x7u" 
-DEFAULT TABLESPACE users
-QUOTA UNLIMITED ON users; 
+    -- USER hotel_app
+    EXECUTE IMMEDIATE '
+        CREATE USER hotel_app IDENTIFIED by "3X?c5K/w6E3d%R{x7u" 
+        DEFAULT TABLESPACE users
+        QUOTA UNLIMITED ON users';
 
-GRANT CONNECT TO hotel_app; ;
--- cf le script grant_existing_privileges.sql et grant_trigger.sql
-
-
-
--- -- Audit
-
--- AUDIT INSERT, UPDATE, DELETE ON users BY hotel_app;
--- AUDIT SESSION BY hotel_app;
--- AUDIT SESSION BY hotel_app WHENEVER NOT SUCCESSFUL;
-
--- AUDIT CREATE, ALTER, DROP ON SCHEMA hotel_admin;
+    EXECUTE IMMEDIATE 'GRANT CONNECT TO hotel_app';
+    -- cf le script grant_existing_privileges.sql et grant_trigger.sql
 
 
--- CREATE PROFILE hotel_app_profile LIMIT
---     SESSIONS_PER_USER 50,        -- Nombre maximal de connexions dans le pool
---     CPU_PER_SESSION UNLIMITED,   -- Pas de limite sur le temps CPU total par session
---     CPU_PER_CALL 5000,           -- Limite raisonnable (5 secondes) par requête
---     CONNECT_TIME UNLIMITED,      -- Pas de limite sur la durée totale de la session
---     IDLE_TIME UNLIMITED;         -- Pas de déconnexion automatique pour inactivité
 
--- ALTER USER hotel_app PROFILE hotel_app_profile;
+    -- -- Audit
+    EXECUTE IMMEDIATE 'CREATE AUDIT POLICY audit_hotel_users ACTIONS ALL';
+    EXECUTE IMMEDIATE 'AUDIT POLICY audit_hotel_users BY hotel_admin, hotel_app';
+    -- SELECT * FROM AUDIT_UNIFIED_POLICIES WHERE POLICY_NAME = 'AUDIT_HOTEL_USERS';
 
-SELECT USERNAME, PROFILE FROM DBA_USERS WHERE USERNAME = 'HOTEL_APP';
-SELECT * FROM DBA_PROFILES WHERE PROFILE = 'HOTEL_APP_PROFILE';
+    -- -- Audit
 
+    -- CREATE PROFILE hotel_app_profile LIMIT
+    --     SESSIONS_PER_USER 50,        -- Nombre maximal de connexions dans le pool
+    --     CPU_PER_SESSION UNLIMITED,   -- Pas de limite sur le temps CPU total par session
+    --     CPU_PER_CALL 5000,           -- Limite raisonnable (5 secondes) par requête
+    --     CONNECT_TIME UNLIMITED,      -- Pas de limite sur la durée totale de la session
+    --     IDLE_TIME UNLIMITED;         -- Pas de déconnexion automatique pour inactivité
+
+    -- ALTER USER hotel_app PROFILE hotel_app_profile;
+
+    -- SELECT USERNAME, PROFILE FROM DBA_USERS WHERE USERNAME = 'HOTEL_APP';
+    -- SELECT * FROM DBA_PROFILES WHERE PROFILE = 'HOTEL_APP_PROFILE';
+
+-- Commit transaction if no errors
+    EXECUTE IMMEDIATE 'COMMIT';
+
+EXCEPTION
+    WHEN OTHERS THEN
+        -- Rollback in case of error
+        err_code := SQLCODE;
+        err_msg := SQLERRM;
+        EXECUTE IMMEDIATE 'ROLLBACK';
+        DBMS_OUTPUT.PUT_LINE('Error Code: ' || err_code || ' - Message: ' || err_msg);
+END;
+/
 
 SPOOL OFF;
 
@@ -77,3 +94,4 @@ SPOOL OFF;
 
 -- oracle wallet manager
 -- les requete V$XXXX
+
