acaec0e34d13bdebffaac9bca90583844c66f1d2
LeMarechalDeFer <blanchot@et.esiea.fr>
2024-11-23 16:16:32 +0100

    script init db

---
+++ FormationSQL/Oracle/HLL/03_seed_data.sql
@@ -0,0 +1,72 @@
+-- -- Insertion de données initiales
+
+-- INSERT INTO rooms (room_type, price_per_night, status)
+-- VALUES ('Single', 100, 'available'),
+--        ('Double', 150, 'available'),
+--        ('Suite', 300, 'maintenance');
+
+-- INSERT INTO clients (first_name, last_name, email, phone_number)
+-- VALUES ('John', 'Doe', 'johndoe@example.com', '1234567890');
+
+-- INSERT INTO translations (lang_code, translations)
+-- VALUES ('en', '{"welcome": "Welcome", "room": "Room"}'),
+--        ('fr', '{"welcome": "Bienvenue", "room": "Chambre"}');
+
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('user');
+
+INSERT INTO users ()
+
+
+INSERT INTO frequency (name) VALUES ('once');
+INSERT INTO frequency (name) VALUES ('daily');
+INSERT INTO frequency (name) VALUES ('weekly');
+INSERT INTO frequency (name) VALUES ('monthly');
+INSERT INTO frequency (name) VALUES ('yearly');
+
+
+INSERT INTO nationality ()
+
+
+
+INSERT INTO status () 
+
+-- Haute, basse, estivale. Date a changer
+INSERT INTO seasons (name) VALUES ('High');
+INSERT INTO seasons (name) VALUES ('Low');
+INSERT INTO seasons (name) VALUES ('Summer');
+
+-- mettre les bonnes dates
+INSERT INTO season_periods (season_id, start_date, end_date) VALUES (1, '2021-06-01', '2021-08-31');
+INSERT INTO season_periods (season_id, start_date, end_date) VALUES (1, '2021-06-01', '2021-08-31');
+INSERT INTO season_periods (season_id, start_date, end_date) VALUES (2, '2021-09-01', '2021-12-31');
+INSERT INTO season_periods (season_id, start_date, end_date) VALUES (3, '2021-01-01', '2021-05-31');
+
+-- INSERT INTO translations (entity_name, entity_id, lang_code, field_name, translation)
+-- VALUES 
+--     ('seasons', 1, 'en', 'name', 'High'),
+--     ('seasons', 1, 'fr', 'name', 'Haute'),
+--     ('seasons', 2, 'en', 'name', 'Low'),
+--     ('seasons', 2, 'fr', 'name', 'Basse'),
+--     ('seasons', 3, 'en', 'name', 'Summer'),
+--     ('seasons', 3, 'fr', 'name', 'Estivale');
+
+INSERT INTO room_type ()
+
+
+INSERT INTO hotel_options ()
+
+
+INSERT INTO hotel_services ()
+
+INSERT INTO client_types ()
+
+
+INSERT INTO payment_method ()
+
+
+INSERT INTO hotel_services_schedule ()
+
+
+INSERT INTO room_rates ()
--- FormationSQL/Oracle/HLL/db-init.sh
+++ FormationSQL/Oracle/HLL/db-init.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-sqlplus hotel_admin/securepassword@orcl <<EOF
-@01_user_roles.sql
-@02_tables.sql
-@03_views.sql
-@04_procedures_and_functions.sql
-@05_parameters_and_config.sql
-@06_indexes.sql
-@07_security.sql
-@08_seed_data.sql
-@09_jobs_and_automation.sql
+sqlplus / as sysdba <<EOF
+@/opt/oracle/scripts/setup/01_user_roles.sql
+@/opt/oracle/scripts/setup/02_tables.sql
+@/opt/oracle/scripts/setup/grant_existing_privileges.sql
 EXIT;
 EOF
